7e5a157cd5c2642189599944ffb35b2c5adeae80
github username <github email address>
2020-02-03 23:03:43 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,47 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+constructor() {
+  super()
+
+  this.state = {
+     pizzas : [],
+     myPizza : null
+  }
+}
+
+componentDidMount() {
+  fetch('http://localhost:3000/pizzas')
+  .then((res) => res.json())
+  .then(pizzas => this.setState({
+    pizzas : pizzas
+  }))
+}
+
+handleEditBtn = (pizza) => {
+  this.setState({
+    myPizza : pizza
+  })
+  console.log(this.state.myPizza)
+}
+
+handleEditPizza = (pizza) => {
+  console.log("working")
+}
+
+handleEditTopping = (event) => {
+  console.log(event.target.value)
+  
+}
+
+
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm myPizza={this.state.myPizza} subEditTopping={this.handleEditTopping} onNewPizza={this.handlePizzaChange}/>
+        <PizzaList pizzas={this.state.pizzas} editPizza={this.handleEditBtn}/>
       </Fragment>
     );
   }
